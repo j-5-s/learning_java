f8e57f1e8a5bc083e10da1972dcafbaca8416619
James Charlesworth <james_h_charlesworth@homedepot.com>
2014-06-08 19:16:57 -0400

    classified

--- LearningJava/src/com/homedepot/headfirst/PrimeNumbers.java
+++ LearningJava/src/com/homedepot/headfirst/PrimeNumbers.java
@@ -1,22 +1,86 @@
 package com.homedepot.headfirst;
 
+import java.util.*;
+
+/**
+ * Simple class for working with prime numbers
+ * For chapter 1 of Learning Java
+ * @author jamescharlesworth
+ *
+ */
 public class PrimeNumbers {
+
 	public static void main(String[] args) {
-		int i = 1;
-		while (i < 100) {
-			int n = 2;
-			boolean isPrime = true;
-			while (n < i) {
-				if (i % n == 0) {
-					isPrime = false;
-					break;
-				}
-				n++;
+		PrimeNumbers p = new PrimeNumbers();
+		List<Integer> primes = p.getPrimeNumbers(1, 100);
+		System.out.print(p.toString(primes, " "));
+	}
+
+	/**
+	 * returns a list of prime numbers for a given set.
+	 * 
+	 * @param fromInt
+	 * @param toInt
+	 * @return List<Integer>
+	 */
+	public List<Integer> getPrimeNumbers(Integer fromInt, Integer toInt) {
+		List<Integer> primeNumbers = new ArrayList<Integer>();
+
+		while (fromInt < toInt) {
+			if (this.isPrime(fromInt)) {
+				primeNumbers.add(fromInt);
 			}
-			if (isPrime) { 
-				System.out.print(i + " ");
+			fromInt++;
+		}
+		return primeNumbers;
+	}
+
+	/**
+	 * Determines if the number is prime.
+	 * 
+	 * @param number
+	 * @return boolean
+	 */
+	public boolean isPrime(int number) {
+		boolean isPrime = true;
+		int i = 2;
+		number = Math.abs(number);
+		while (i < number) {
+			if (number % i == 0) {
+				isPrime = false;
+				break;
 			}
 			i++;
 		}
+		return isPrime;
 	}
+
+	/**
+	 * Turns the list of numbers to a string;
+	 * 
+	 * @param list
+	 * @return String
+	 */
+	public String toString(List<Integer> list) {
+		return this.toString(list, " ");
+	}
+
+	/**
+	 * Turns the list of numbers to a string;
+	 * 
+	 * @param list
+	 * @param deliminator
+	 * @return
+	 */
+	public String toString(List<Integer> list, String deliminator) {
+		String listString = "";
+		for (int i = 0; i < list.size(); i++) {
+			listString += list.get(i);
+			if (i < list.size() - 1) {
+				listString += deliminator;
+			}
+			;
+		}
+		return listString;
+	};
 }
